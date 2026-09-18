4a5170f5732a6712a10cb335c79af52b336704df
gizmore <gizmore@wechall.net>
2026-09-18 04:05:41 +0200

    'more korean'

---
+++ lang/birthday_ko.php
@@ -0,0 +1,28 @@
+<?php
+namespace GDO\Birthday\lang;
+
+return [
+	'birthday' => '생년월일',
+	'birthdate' => '생년월일',
+	'announce_my_birthday' => '내 생일을 다른 사람에게 알리나요?',
+	'announce_me_birthdays' => '생일이 있는 사람이 있으면 나에게 이메일을 보내시겠습니까?',
+	'age_visible' => '당신의 나이를 보여주세요',
+	'birthdate_visible' => '당신의 생일을 보여주세요',
+	'cfg_birthday_alerts' => '생일 뉴스를 사용하시겠습니까?',
+	'cfg_global_min_age' => '최소 전체 사이트의 연령',
+	'cfg_method_min_age' => '최소 세분화된 방법의 시대',
+	'mt_birthday_verifyage' => '연령 제한 액세스',
+	'info_age_verify' => '%s세 이상인지 확인하려면 생일을 알려주세요.',
+	'err_age_verify' => '귀하는 연령이 충분하지 않기 때문에 이 내용을 볼 수 없습니다. %s세 이상이어야 합니다.',
+	'msg_birthdate_session_set' => '귀하의 생년월일은 %s(으)로 설정되었습니다. 귀하의 나이는 %s세로 설정되었습니다.',
+	'mailsubj_birthdays' => '%s: 생일입니다!',
+	'mailline_birthdays' => '%s는 오늘 %s살이 되었습니다!',
+	'mailbody_birthdays' => '친애하는 %s님,
+
+%s의 다음 사람들이 오늘 생일을 맞았습니다:
+
+%s
+
+친절한 감사
+%2$s 시스템',
+];
